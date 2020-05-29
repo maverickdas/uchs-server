994ebef24be2275e5de138cea71165cc88fdf0d7
Abhirup Das <abhidash@outlook.com>
2020-05-29 08:32:25 +0530

    fixed UUID validation with hyphens

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ import uchs_exceptions as exs
 def is_valid_uuid(string, version_num=1):
     assert version_num <= 4, "UUID versions upto 4 supported"
     try:
+        string  = "".join(string.split("-"))
+        print(string)
         val = UUID(string, version=version_num)
     except ValueError:
         return False
